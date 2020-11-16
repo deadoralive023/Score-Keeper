85cb2429edec9ce66f761aa44e62aef7ed33217c
Uon Liaquat <antelopes06@gmail.com>
2020-11-16 18:02:45 +0500

    Added Drop Down and custom winning functionality

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
-const reset = document.querySelector('#reset');
+const resetButton = document.querySelector('#reset');
+const winingScoreSelect = document.querySelector('#playto');
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
 
 let p1Score = 0, p2Score = 0;
-let winningScore = 5;
+let winningScore = 3;
 let isGameOver = false;
 
 p1Button.addEventListener('click', function(){
@@ -14,6 +15,8 @@ p1Button.addEventListener('click', function(){
         p1Display.textContent = p1Score;
     }
     if(p1Score === winningScore){
+        p1Display.classList.add('winner');
+        p2Display.classList.add('loser');
         isGameOver = true;
     }
 })
@@ -24,6 +27,26 @@ p2Button.addEventListener('click', function(){
         p2Display.textContent = p2Score;
     }
     if(p2Score === winningScore){
+        p2Display.classList.add('winner');
+        p1Display.classList.add('loser');
         isGameOver = true;
     }
 })
+
+resetButton.addEventListener('click', reset)
+
+winingScoreSelect.addEventListener('change', function(){
+    winningScore = parseInt(this.value);
+    reset();
+})
+
+function reset(){
+    isGameOver = false;
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove('winner', 'loser');
+    p2Display.classList.remove('winner', 'loser');
+}
+
