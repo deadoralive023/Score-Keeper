b34d625738b3742ab3c1c38c1ad73a6a5bfd8781
Uon Liaquat <antelopes06@gmail.com>
2020-11-16 18:27:29 +0500

    Added Stlyes using Bulma
    Added Card-View using Bulma
    Addded different styles and colors using Bulma
    Added Ping Pong Image on top

--- app.js
+++ app.js
@@ -15,9 +15,11 @@ p1Button.addEventListener('click', function(){
         p1Display.textContent = p1Score;
     }
     if(p1Score === winningScore){
-        p1Display.classList.add('winner');
-        p2Display.classList.add('loser');
+        p1Display.classList.add('has-text-success');
+        p2Display.classList.add('has-text-danger');
         isGameOver = true;
+        p1Button.disabled = true;
+        p2Button.disabled = true;
     }
 })
 
@@ -27,9 +29,11 @@ p2Button.addEventListener('click', function(){
         p2Display.textContent = p2Score;
     }
     if(p2Score === winningScore){
-        p2Display.classList.add('winner');
-        p1Display.classList.add('loser');
+        p2Display.classList.add('has-text-success');
+        p1Display.classList.add('has-text-danger');
         isGameOver = true;
+        p1Button.disabled = true;
+        p2Button.disabled = true;
     }
 })
 
@@ -46,7 +50,9 @@ function reset(){
     p2Score = 0;
     p1Display.textContent = 0;
     p2Display.textContent = 0;
-    p1Display.classList.remove('winner', 'loser');
-    p2Display.classList.remove('winner', 'loser');
+    p1Display.classList.remove('has-text-success', 'has-text-danger');
+    p2Display.classList.remove('has-text-success', 'has-text-danger');
+    p1Button.disabled = false;
+    p2Button.disabled = false;
 }
 
